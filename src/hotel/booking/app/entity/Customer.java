97facfae61ef8dd1d7248dede89997c7be6c9c11
Mirzet Dolic <mdolic1@dmacc.edu>
2018-04-24 14:10:04 -0500

    Update Customer.java

--- src/hotel/booking/app/entity/Customer.java
+++ src/hotel/booking/app/entity/Customer.java
@@ -1,178 +1,107 @@
 package hotel.booking.app.entity;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.springframework.beans.factory.annotation.Value;
-
 @Entity
-@Table(name="customers")
+@Table(name = "customers")
 public class Customer {
-	
-	
+
 	@Id
-	@Column(name="id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
-	
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String firstname;
-	
-	
 
-	@Column(name="last_name")
+	@Column(name = "last_name")
 	private String lastname;
-	
 
-	@Column(name="street")
-	private String Street;
+	@OneToOne
+	@JoinColumn(name = "room_id")
+	private Room roomId;
 
-
-
-	@Column(name="city")
-	private String City;
-	
-	@Column(name="state")
-	private String state;
-	
-	
-	
-
-	@Column(name="email")
-	private String email;
-	
-	
-
-	@Column(name="phone")
-	private String phone;
-	
-	
-
-	@Column(name="payment")
-	private String payment;
-	
-	@Column(name="zip")
-	private String Zip;
-	
-	@Column(name="customerNotes")
-	private String customerNotes;
-	
-	public String getCustomerNotes() {
-		return customerNotes;
+	/**
+	 * @return the roomId
+	 */
+	public Room getRoomId() {
+		return roomId;
 	}
 
-	public void setCustomerNotes(String customerNotes) {
-		this.customerNotes = customerNotes;
+	/**
+	 * @param roomId
+	 *            the roomId to set
+	 */
+	public void setRoomId(Room roomId) {
+		this.roomId = roomId;
 	}
 
-	
-	public Customer(){
-	}
-	
-		//getter & setter
-			
+	/**
+	 * @return the id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @param id
+	 *            the id to set
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	public String getfirstname() {
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
 		return firstname;
 	}
 
-	public void setfirstname(String firstname) {
+	/**
+	 * @param firstname
+	 *            the firstname to set
+	 */
+	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
 
-	public String getlastname() {
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
 		return lastname;
 	}
 
-	public void setlastname(String lastname) {
+	/**
+	 * @param lastname
+	 *            the lastname to set
+	 */
+	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
 
-	public String getStreet() {
-		return Street;
-	}
-
-	public void setStreet(String Street) {
-		this.Street = Street;
-	}
-
-	public String getCity() {
-		return City;
-	}
-
-	public void setCity(String City) {
-		this.City = City;
-	}
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String Email) {
-		this.email = Email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setphone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getPayment() {
-		return payment;
-	}
-
-	public void setPayment(String payment) {
-		this.payment = payment;
-	}
-
-
-	public String getZip() {
-		return Zip;
-	}
-
-	public void setZip(String zip) {
-		Zip = zip;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", Street=" + Street
-				+ ", City=" + City + ", email=" + email + ", phone=" + phone + ", payment=" + payment + ", Zip=" + Zip
+		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", roomId=" + roomId
 				+ "]";
 	}
 
-
-
-
-	
 }
